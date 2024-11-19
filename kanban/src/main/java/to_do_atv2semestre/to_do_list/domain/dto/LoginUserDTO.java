667553ba32830b1b3a7fd591fee729a7485c43ad
gabriel romanini <126634833+gabmonikx04@users.noreply.github.com>
2024-11-18 23:52:37 -0300

    Delete kanban directory

--- kanban/src/main/java/to_do_atv2semestre/to_do_list/domain/dto/LoginUserDTO.java
+++
@@ -1,9 +0,0 @@
-package to_do_atv2semestre.to_do_list.domain.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginUserDTO {
-    private String username;
-    private String password;
-}
--- kanban/src/main/java/to_do_atv2semestre/to_do_list/domain/enums/Status.java
+++
@@ -1,7 +0,0 @@
-package to_do_atv2semestre.to_do_list.domain.enums;
-
-public enum Status {
-    A_Fazer,
-    Em_Progresso,
-    Concluido
-}
